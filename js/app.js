302d24e167f4479c366fb21ca80d26743179904c
unknown <addrake@L-PRG-00821500.corp.ebay.com>
2016-07-12 08:49:16 +0200

    Second Commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var C = 0;
+var F = 0;
+var startbound = 32;
+var endbound = 100;
+var increment = 3;
+
+$(document).ready(function () {
+
+    function getC(F) {
+        return (F - 32) * 5/9;
+    }
+
+
+    
+
+
+
+});
