08bfb9f7bd7db77cfc95d009c9f06efa6ed7bf70
unknown <addrake@L-PRG-00821500.corp.ebay.com>
2016-07-12 14:15:19 +0200

    Index.html and app.js updated

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
 
 var C = 0;
 var F = 0;
+console.log(F);
 var startbound = 32;
 var endbound = 100;
-var increment = 3;
+var increment = 1;
+var htmlout = "";
 
 $(document).ready(function () {
 
@@ -14,17 +16,32 @@ $(document).ready(function () {
 
     $('#submitButton').click(function () {
 
+        startbound = parseFloat($('#start').val());
+        endbound = parseFloat($('#end').val());
+        increment = parseFloat($('#inc').val());
+
+
         F = startbound;
+        htmlout = '<table class="table table-striped">';
+        htmlout = htmlout + "<thead><tr><th>Celsius</th><th>Fahrenheit</th></tr></thead>";
+
         do
         {
+            htmlout = htmlout + "<tr>";
             C = getC(F);
+            htmlout = htmlout + "<td>" + Math.round(C.toString() * 100) / 100 + "</td>";
+            htmlout = htmlout + "<td>" + F.toString() + "</td>";
+
             console.log(C, F);
-            
             F = F + increment;
+            htmlout = htmlout + "</tr>";
+
+
         } while (F < endbound)
 
+        htmlout = htmlout + "</table>";
 
-        $('#details').html(C);
+        $('#details').html(htmlout);
 
     });
     
